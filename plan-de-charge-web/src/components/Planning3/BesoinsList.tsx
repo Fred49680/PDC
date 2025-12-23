e2780cb942eec1fa7e6d4911f21b18b6ce016ec3
Frederic Baudry <fredericbaudry49680@gmail.com>
2025-12-23 13:02:27 +0100

    ui: reduire grille tuiles a 4 colonnes maximum
    - Change de 6 a 4 tuiles par ligne pour meilleure lisibilite

--- plan-de-charge-web/src/components/Planning3/BesoinsList.tsx
+++ plan-de-charge-web/src/components/Planning3/BesoinsList.tsx
@@ -252,7 +252,7 @@ export function BesoinsList({
                       </span>
                     </div>
                     {/* Grille de tuiles pour ce groupe */}
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-3">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
                       {groupe.besoins.map((besoin) => (
                         <BesoinCard
                           key={besoin.id}
